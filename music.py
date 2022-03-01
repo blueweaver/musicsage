e636179dc6e40aa23d1cf494e3760f3cd9f6659d
blueweaver <brandonweaver2011@hotmail.com>
2022-03-01 14:44:54 -0800

    update only using Spotify API

--- music.py
+++ music.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3.9
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import discord
 import json
 from cherrypicker import CherryPicker
 import pandas as pd
 from discord.ext import commands
-import musicbrainzngs
+# The following code is used from the article on how to link the spotipy module to the spotify api
+# https://medium.com/@maxtingle/getting-started-with-spotifys-api-spotipy-197c3dc6353b
+cid = '*******'
+secret = '********'
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-TOKEN = '*************'
+TOKEN = '*******'
 help_command = commands.DefaultHelpCommand(
     no_category = 'Commands'
 )
@@ -19,14 +26,13 @@ async def on_ready():
 async def who(ctx, message):
     if ctx.author == client.user:
         return
-    musicbrainzngs.set_useragent("musicSage", "1.0", "https://github.com/blueweaver/musicsage")
-    musicbrainzngs.set_format(fmt='json')
     print(message)
-    res = musicbrainzngs.search_recordings(message, limit=1, strict=True)
-    picker = CherryPicker(res)
-    flat = picker['recordings'].flatten().get()
-    df = pd.DataFrame(flat)
+    query = 'track: ' + message
+    res = sp.search(q=query, limit=1, type='track')
+    df = pd.json_normalize(res["tracks"]["items"][0]["artists"][0])
+    df.info()
+    print(df['name'].values[0])
     info = "Artist: "
-    info += str(df['artist-credit_0_name'].values[0])
+    info += str(df['name'].values[0])
     await ctx.channel.send(info)
 client.run(TOKEN)

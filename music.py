53f8db5cdc728f856a433dc519ea3f80a7bb5b22
blueweaver <brandonweaver2011@hotmail.com>
2022-03-07 23:47:14 -0800

    added more detail to the who command

--- music.py
+++ music.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.9
+from matplotlib import artist
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import discord
@@ -8,12 +9,12 @@ import pandas as pd
 from discord.ext import commands
 # The following code is used from the article on how to link the spotipy module to the spotify api
 # https://medium.com/@maxtingle/getting-started-with-spotifys-api-spotipy-197c3dc6353b
-cid = '*******'
+cid = '********'
 secret = '********'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-TOKEN = '*******'
+TOKEN = '********'
 help_command = commands.DefaultHelpCommand(
     no_category = 'Commands'
 )
@@ -29,10 +30,20 @@ async def who(ctx, message):
     print(message)
     query = 'track: ' + message
     res = sp.search(q=query, limit=1, type='track')
-    df = pd.json_normalize(res["tracks"]["items"][0]["artists"][0])
-    df.info()
-    print(df['name'].values[0])
-    info = "Artist: "
-    info += str(df['name'].values[0])
+    dfArtist = pd.json_normalize(res["tracks"]["items"][0]["artists"])
+    dfTrack = pd.json_normalize(res["tracks"]["items"][0])
+    dfArtist.info()
+    info = "The song " + dfTrack['name'][0] + " is by the "
+    if(dfArtist['name'].size == 2):
+        info += "artists " + dfArtist['name'].values[0] + " and " + dfArtist['name'].values[1]
+    elif(dfArtist['name'].size > 1):
+        info += "artists "
+        for i in range(dfArtist['name'].size):
+            if(i != dfArtist['name'].size-1):
+                info += dfArtist['name'].values[i] + ", "
+            else:
+                info += "and " + dfArtist['name'].values[i]
+    else:
+        info += "artist " + dfArtist['name'].values[0]
     await ctx.channel.send(info)
 client.run(TOKEN)

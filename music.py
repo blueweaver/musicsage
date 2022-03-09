067a06e3c57b8716abd8ae6ac6441d7b630d2533
blueweaver <brandonweaver2011@hotmail.com>
2022-03-08 22:34:26 -0800

    added more complex queries

--- music.py
+++ music.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3.9
+from pickle import FALSE
+from unicodedata import name
 from matplotlib import artist
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -9,12 +11,12 @@ import pandas as pd
 from discord.ext import commands
 # The following code is used from the article on how to link the spotipy module to the spotify api
 # https://medium.com/@maxtingle/getting-started-with-spotifys-api-spotipy-197c3dc6353b
-cid = '********'
-secret = '********'
+cid = '******'
+secret = '*********'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-TOKEN = '********'
+TOKEN = '*********'
 help_command = commands.DefaultHelpCommand(
     no_category = 'Commands'
 )
@@ -23,6 +25,7 @@ client = commands.Bot(command_prefix='!', help_command = help_command)
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+#track query examples
 @client.command()
 async def who(ctx, message):
     if ctx.author == client.user:
@@ -30,6 +33,10 @@ async def who(ctx, message):
     print(message)
     query = 'track: ' + message
     res = sp.search(q=query, limit=1, type='track')
+    if(len(res["tracks"]["items"]) == 0):
+        info = "Sorry! I cannot find what you are looking for. please try again."
+        await ctx.channel.send(info)
+        return
     dfArtist = pd.json_normalize(res["tracks"]["items"][0]["artists"])
     dfTrack = pd.json_normalize(res["tracks"]["items"][0])
     dfArtist.info()
@@ -45,5 +52,90 @@ async def who(ctx, message):
                 info += "and " + dfArtist['name'].values[i]
     else:
         info += "artist " + dfArtist['name'].values[0]
+    info += "\n" + dfTrack['album.images'][0][1]['url']
+    await ctx.channel.send(info)
+
+@client.command()
+async def explicit(ctx, message):
+    if ctx.author == client.user:
+        return
+    print(message)
+    query = 'track: ' + message
+    res = sp.search(q=query, limit=1, type='track')
+    if(len(res["tracks"]["items"]) == 0):
+        info = "Sorry! I cannot find what you are looking for. please try again."
+        await ctx.channel.send(info)
+        return
+    dfArtist = pd.json_normalize(res["tracks"]["items"][0]["artists"])
+    dfTrack = pd.json_normalize(res["tracks"]["items"][0])
+    dfTrack.info()
+    info = "The song " + dfTrack['name'][0] + " by the "
+    if(dfArtist['name'].size == 2):
+        info += "artists " + dfArtist['name'].values[0] + " and " + dfArtist['name'].values[1]
+    elif(dfArtist['name'].size > 1):
+        info += "artists "
+        for i in range(dfArtist['name'].size):
+            if(i != dfArtist['name'].size-1):
+                info += dfArtist['name'].values[i] + ", "
+            else:
+                info += "and " + dfArtist['name'].values[i]
+    else:
+        info += "artist " + dfArtist['name'].values[0]
+    if(dfTrack['explicit'].bool() == False):
+        info += " is not explicit"
+    else:
+        info += " is explicit"
+    info += "\n" + dfTrack['album.images'][0][1]['url']
+    await ctx.channel.send(info)
+    
+@client.command()
+async def album(ctx, message):
+    if ctx.author == client.user:
+        return
+    print(message)
+    query = 'track: ' + message
+    res = sp.search(q=query, limit=1, type='track')
+    if(len(res["tracks"]["items"]) == 0):
+        info = "Sorry! I cannot find what you are looking for. please try again."
+        await ctx.channel.send(info)
+        return
+    dfArtist = pd.json_normalize(res["tracks"]["items"][0]["artists"])
+    dfTrack = pd.json_normalize(res["tracks"]["items"][0])
+    dfTrack.info()
+    info = "The song " + dfTrack['name'][0] + " by the "
+    if(dfArtist['name'].size == 2):
+        info += "artists " + dfArtist['name'].values[0] + " and " + dfArtist['name'].values[1]
+    elif(dfArtist['name'].size > 1):
+        info += "artists "
+        for i in range(dfArtist['name'].size):
+            if(i != dfArtist['name'].size-1):
+                info += dfArtist['name'].values[i] + ", "
+            else:
+                info += "and " + dfArtist['name'].values[i]
+    else:
+        info += "artist " + dfArtist['name'].values[0]
+    info += " is on the album " + dfTrack['album.name'].values[0]
+    info += "\n" + dfTrack['album.images'][0][1]['url']
+    await ctx.channel.send(info)
+
+#artist query examples
+@client.command()
+async def popularSongs(ctx, message):
+    if ctx.author == client.user:
+        return
+    print(message)
+    query = 'artist: ' + message
+    res = sp.search(q=query, type='artist')
+    if(len(res['artists']['items']) == 0):
+        info = "Sorry! I cannot find what you are looking for. please try again."
+        await ctx.channel.send(info)
+        return
+    topSongs = sp.artist_top_tracks(res['artists']['items'][0]['id'], country='US')
+    dfTrack = pd.json_normalize(topSongs['tracks'])
+    dfTrack.info()
+
+    info = "Here are the top 10 songs by " + dfTrack['artists'][0][0]['name'] + ":\n"
+    for i in range(len(dfTrack['name'])):
+        info += "    " + str(i+1) + ". " + dfTrack['name'][i] + "\n"
     await ctx.channel.send(info)
 client.run(TOKEN)
